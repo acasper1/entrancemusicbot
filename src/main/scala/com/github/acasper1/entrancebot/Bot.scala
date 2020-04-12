09ebc14a8296262380ece04768b59cf86dc55034
Adrian Casper <acasper1@msn.com>
2020-04-12 03:18:40 -0400

    Simple configuration & logging

--- src/main/scala/com/github/acasper1/entrancebot/Bot.scala
+++ src/main/scala/com/github/acasper1/entrancebot/Bot.scala
@@ -1,9 +1,14 @@
 package com.github.acasper1.entrancebot
 
 import discord4j.core.{DiscordClient, DiscordClientBuilder}
-
+import org.slf4j.{Logger, LoggerFactory}
 
 object Bot extends App {
-  val discordToken = ""
+
+  val LOGGER: Logger = LoggerFactory.getLogger(this.getClass)
+
+  // TODO: use a more robust method to handle bot configuration
+  val discordToken = args(0)
   val discordClient: DiscordClient = DiscordClientBuilder.create(discordToken).build()
+  discordClient.login().block()
 }
